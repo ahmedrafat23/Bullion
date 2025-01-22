b757380f5c6a7c5d3244e0f862706f21befe7cf2
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2025-01-22 15:01:45 +0200

    22/1/2025/1/2025

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,9 @@
 from .app import app
 from .config import Config
 from .data_fetcher import fetch_data
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')  # Import configuration
+    return app
