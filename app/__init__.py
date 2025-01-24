f5cee262cc067afe57c13040948dada4221d58f4
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2025-01-25 01:02:27 +0200

    Flask Application Initialization

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')  # Import configuration
+    
+     # Import routes here or register blueprints
+    from .app import app as main_app
+    app.register_blueprint(main_app)
+    
     return app
